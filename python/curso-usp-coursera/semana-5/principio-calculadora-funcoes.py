7856514613399f495cfc873a87ebb85e06941238
Sidnei Weber <sidnei.weber@gmail.com>
2017-04-11 20:34:08 -0300

    Create principio-calculadora-funcoes.py

---
+++ python/curso-usp-coursera/semana-5/principio-calculadora-funcoes.py
@@ -0,0 +1,13 @@
+#!/bin/python
+
+def soma (x, y):
+  return x + y
+  
+def menos (x, y):
+  return x - y
+  
+#print(soma(1, 5))
+n1=int(input("Digite o 1 valor: "))
+n2=int(input("Digite o 2 valor: "))
+
+print("Soma dos valores: ", soma(n1, n2))
