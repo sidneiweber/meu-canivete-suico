e96bfc2fac2e21888ca6befa0ae10f7b0268990e
Sidnei Weber <sidnei.weber@gmail.com>
2017-04-25 22:00:23 -0300

    Create imprime-retangulo.py

---
+++ python/curso-usp-coursera/semana-7/imprime-retangulo.py
@@ -0,0 +1,8 @@
+largura=int(input("Digite a largura:"))
+altura=int(input("Digite a altura:"))
+contlargura=1
+contaltura=1
+
+while contaltura <= altura:
+  print("#" * largura)
+  contaltura=contaltura+1
