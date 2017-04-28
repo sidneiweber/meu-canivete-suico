fed8dba47eb0793241687767c47e8c1b804cbe49
Sidnei Weber <sidnei.weber@gmail.com>
2017-04-27 21:51:35 -0300

    Create remove-repetidos-lista.py

---
+++ python/curso-usp-coursera/semana-8/remove-repetidos-lista.py
@@ -0,0 +1,13 @@
+def remove_repetidos(lista):
+    l = []
+    for i in lista:
+        if i not in l:
+            l.append(i)
+    l.sort()
+    return l
+
+
+lista=[20,3,7,5,11,5,1]
+
+lista = remove_repetidos(lista)
+print (lista)
